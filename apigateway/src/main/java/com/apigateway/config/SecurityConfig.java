2cdd99a1c78fc2b049c809298c7744c817920852
User <shubham.mali@neosoftmail.com>
2023-11-16 19:59:47 +0530

    "6th commit"

---
+++ apigateway/src/main/java/com/apigateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+//project is based on web flux project not on web mvc project
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity)
+    {
+        serverHttpSecurity
+                .csrf()
+                .disable()
+                .authorizeExchange(exchange->exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();
+
+    }
+}
