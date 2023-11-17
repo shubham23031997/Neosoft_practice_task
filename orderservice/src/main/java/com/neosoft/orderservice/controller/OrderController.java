3d340bc7aad3de8b0d9707c032ad3f183a0a60d7
User <shubham.mali@neosoftmail.com>
2023-11-17 20:25:34 +0530

    "6th commit"

--- orderservice/src/main/java/com/neosoft/orderservice/controller/OrderController.java
+++ orderservice/src/main/java/com/neosoft/orderservice/controller/OrderController.java
@@ -2,11 +2,10 @@ package com.neosoft.orderservice.controller;
 
 import com.neosoft.orderservice.dto.OrderRequest;
 import com.neosoft.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/Order")
@@ -15,9 +14,14 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Created Successfully";
     }
 
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops! something went wrong, please order after some time!";
+    }
 }
