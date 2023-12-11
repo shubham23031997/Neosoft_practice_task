8e43e93dbd3f4c10750ed34d1992fb7fc87b2f4e
User <shubham.mali@neosoftmail.com>
2023-12-11 19:50:06 +0530

    header created

---
+++ angular-ekart/src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-header',
+
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
+})
+export class HeaderComponent{
+    //class of component is same as component name and suffix with component
+}
