af519c236eb3ae6fadc53bd2d32db5a1c5e3049f
User <shubham.mali@neosoftmail.com>
2023-12-12 21:30:46 +0530

    "3rd commit"

--- angular-ekart/src/app/app.component.ts
+++ angular-ekart/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 
+import { TopHeaderComponent } from './top-header/top-header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, TopHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
